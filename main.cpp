95f492da7832c47649febed25457bb78f4323b63
ZachLand <62918914+ZachLand@users.noreply.github.com>
2020-11-03 11:17:07 -0600

    Added comments, fixed variables and conditions

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // Prototypes
 double getValue();
-int getTime(int&, int&, string&);
+void getTime(int&, int&, string&);
 void milesDrivenCalc(const double&, int&, double&);
 void getFees(const double&, double&, double&, int);
 void meals(int, int, int, string, string, double&, double&);
@@ -27,8 +27,8 @@ int main ()
     cout << "days: ";
     days = getValue();
 
-    /* The time of departure on the first day of the trip and 
-        the time of arrival back home on the last day of the trip */
+    /* The time of departure of the trip and 
+        the time of arrival back home of the trip */
     // DONE
     int timeOfDepartureHr, timeOfDepartureMin, timeOfArrivalHr, timeOfArrivalMin;
     string am_pm_depart, am_pm_arrive;
@@ -55,8 +55,8 @@ int main ()
         should be have this number round up so that they are charged
         for the mile as a whole? I know companies do this a lot.*/
     // DONE
-    double allowanceVeh;
-    int milesDriven;
+    double allowanceVeh = 0;
+    int milesDriven = 0;
     const double ALLOWANCE_VEHICLE = 0.58;
     milesDrivenCalc(ALLOWANCE_VEHICLE, milesDriven, allowanceVeh);
 
@@ -87,11 +87,11 @@ int main ()
     cout << "hotel: $";
     getFees(ALLOWANCE_HOTEL, feeHotel, feeHotelExcess, days);
 
-    /* The cost of each meal eaten. On the first day of the trip,
+    /* The cost of each meal eaten. of the trip,
         breakfast is allowed as an expense if the time of departure
         is before 7 a.m. Lunch is allowed if the time of departure is
         before noon. Dinner is allowed if the time of departure is before
-        6 p.m. On the last day of the trip, breakfast is allowed if the
+        6 p.m. of the trip, breakfast is allowed if the
         time of arrival is after 8 a.m. Lunch is allowed if the time of
         arrival is after 1 p.m. Dinner is allowed if the time of arrival
         is after 7 p.m. The program should only ask for the costs of
@@ -99,7 +99,8 @@ int main ()
         $12 for lunch, and $20 for dinner. Anything in excess of this
         must be paid by the employee.)*/
     double allowanceTotal, allowanceExceeded;
-    meals(days, timeOfDepartureHr, timeOfArrivalHr, am_pm_depart, am_pm_arrive, allowanceTotal, allowanceExceeded);
+    meals(days, timeOfDepartureHr, timeOfArrivalHr, am_pm_depart, am_pm_arrive, 
+            allowanceTotal, allowanceExceeded);
     
 
     printResults(days, timeOfDepartureHr, timeOfDepartureMin, am_pm_depart, 
@@ -114,8 +115,13 @@ double getValue()
 {
     /* I think we will need to change this since we will be dealing
         int and doubles throughout the program.*/
+    // Declare static variable
     static double a;
+
+    // Get input from keyboard
     cin >> a;
+
+    // While not a number nor within range, clear and loop.
     while(a<0 || cin.fail())
     {
         cout << "Error: Invalid value" << endl;
@@ -126,11 +132,14 @@ double getValue()
     return a;
 }
 
-int getTime(int& a, int& b, string& d)
+void getTime(int& a, int& b, string& d)
 {
     static char c; /* We can use time as integers having them separated
                     with ':' */
+    // Get input from keyboard
     cin >> a >> c >> b >> d;
+
+    // While not withing range of hr and mins, clear and loop.
     while(!((a>0 && a<=12) && (b>=0 && b<=59) && 
         (d=="AM"||d=="am"||d=="PM"||d=="pm")))
     {
@@ -139,11 +148,11 @@ int getTime(int& a, int& b, string& d)
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cin >> a >> c >> b >> d;
     }
+    // Change from lowercase to uppercase for same style.
     if(d=="am")
         d = "AM";
     if(d=="pm")
         d = "PM";
-    return 0;
 }
 /******************************************************************************************/
 
@@ -176,7 +185,7 @@ void printResults(int days, int DeHr, int DeMin, string DAmPm, int ArHr, int ArM
     cout << "milesDriven = " << z << endl;
     cout << "allowanceVeh = $" << av << endl;
     cout << "feeParking = $" << fp << endl;
-    cout << "Parking allowance = $" << 20 * 12 << endl;
+    cout << "Parking allowance = $" << days * 12.00 << endl;
     cout << "Was Parking Allowance Exceeded? ";
     if(fpe<=0)
         cout << "No" << endl;
@@ -219,50 +228,61 @@ void getFees(const double& ALLOWANCE, double& fee, double& feeExcess, int days)
     fee = getValue();
     // Calculates the amount the user then has to pay.
     feeExcess = fee - (days * ALLOWANCE);
+    if (feeExcess < 0)
+        feeExcess = 0;
 }
 /******************************************************************************************/
-void meals(int days, int timeOfDepartureHr, int timeOfArrivalHr, string am_pm_depart, string am_pm_arrive, double& allowanceTotal, double& allowanceExceeded)
+void meals(int days, int timeOfDepartureHr, int timeOfArrivalHr, string am_pm_depart, 
+        string am_pm_arrive, double& allowanceTotal, double& allowanceExceeded)
 {
-    double bfast1, lnch1, dnr1, bfast2, lnch2, dnr2, bfastT, lnchT, dnrT, oweb1, oweb2,
-    oweb, owel1, owel2, owel, owed1, owed2, owed, bfast, lnch, dnr, bfastTemp, lnchTemp, dnrTemp,
-    breakfast_total_allowance = 0, lunch_total_allowance = 0, dinner_total_allowance = 0,
-    breakfast_total_exceeded = 0, lunch_total_exceeded = 0, dinner_total_exceeded = 0;
+    // Declare variables and initialize to zero.
+    double bfast, lnch, dnr, breakfast_total_allowance = 0, lunch_total_allowance = 0, 
+    dinner_total_allowance = 0, breakfast_total_exceeded = 0, lunch_total_exceeded = 0,
+    dinner_total_exceeded = 0;
     int i;
 
 /******** FIRST DAY ****************************************************************************/
+    // Conditions for AM on first day.
+    cout << "On day 1" << endl;
     if (am_pm_depart == "am" || am_pm_depart == "AM")
     {
+        // Ask for breakfast, lunch and dinner if betweem midnight and before 7am.
         if (timeOfDepartureHr < 7 || timeOfDepartureHr == 12)
         {
-        cout << "How much did breakfast cost on the first day? $";
-        cin >> bfast;
-        cout << "How much did lunch cost on the first day? $";
-        cin >> lnch;
-        cout << "How much did dinner cost on the first day? $";
-        cin >> dnr;
-        }
-        else if (timeOfDepartureHr >= 7 && timeOfDepartureHr < 12)
-        {
-        cout << "How much did lunch cost on the first day? $";
-        cin >> lnch;
-        cout << "How much did dinner cost on the first day? $";
-        cin >> dnr;
-        }
-    }
-    if (am_pm_depart == "pm" || am_pm_depart == "PM")
-    {
-        if (timeOfDepartureHr < 6 || timeOfDepartureHr == 12)
-        {
-            cout << "How much did dinner cost on the first day? $";
+            cout << "How much did breakfast cost? $";
+            cin >> bfast;
+            cout << "How much did lunch cost? $";
+            cin >> lnch;
+            cout << "How much did dinner cost? $";
             cin >> dnr;
         }
-        else
-        cout << "Time of departure was too late for any meals on the first day!";
+        // Ask for lunch and dinner if between 7am and before noon.
+        else if (timeOfDepartureHr >= 7 && timeOfDepartureHr < 12)
+        {
+            bfast = 0;
+            cout << "How much did lunch cost? $";
+            cin >> lnch;
+            cout << "How much did dinner cost? $";
+            cin >> dnr;
+        }
     }
+    // Conditions for PM on first day.
+    if (am_pm_depart == "pm" || am_pm_depart == "PM")
+    {
+        // Ask for dinner if between 12pm and before 6pm
+        if (timeOfDepartureHr < 6 || timeOfDepartureHr == 12)
+        {
+            bfast = lnch = 0;
+            cout << "How much did dinner cost? $";
+            cin >> dnr;
+        }
+    }
+    // Call function to calculate allowanaces and exceedance for each meal.
     mealComp(bfast, lnch, dnr, breakfast_total_allowance, breakfast_total_exceeded, 
         lunch_total_allowance, lunch_total_exceeded, dinner_total_allowance,
         dinner_total_exceeded);
     /***********Days inbetween*******************************************/
+    // if more than 2 day, ask for all meals in between first and last day.
     for(i = 2; i < days; i++)
     {
         cout << "On day " << i << endl;
@@ -273,51 +293,68 @@ void meals(int days, int timeOfDepartureHr, int timeOfArrivalHr, string am_pm_de
         cout << "How much did dinner cost? $";
         cin >> dnr;
 
+        // Call function to calculate allowanaces and exceedance for each meal.
         mealComp(bfast, lnch, dnr, breakfast_total_allowance, breakfast_total_exceeded, 
         lunch_total_allowance, lunch_total_exceeded, dinner_total_allowance,
         dinner_total_exceeded);
     }
     
 /******* LAST DAY *****************************************************************************/
-    if (am_pm_arrive == "am" || am_pm_arrive == "AM")
+    // Conditions for AM on last day.
+    if (days >= 2)
     {
-        if (timeOfArrivalHr >= 8 && timeOfArrivalHr < 12)
+        cout << "On day " << days << endl;
+        if (am_pm_arrive == "am" || am_pm_arrive == "AM")
         {
-            cout << "How much did breakfast cost on the last day? $";
-            cin >> bfast;
+            // Ask for breakfast if between 8am and midnight.
+            if (timeOfArrivalHr >= 8 && timeOfArrivalHr < 12)
+            {
+                cout << "How much did breakfast cost? $";
+                cin >> bfast;
+                lnch = dnr = 0;
+            }
+            
         }
-        
+        // Conditions for PM on last day.
+        if (am_pm_arrive == "pm" || am_pm_arrive == "PM")
+        {
+            // Ask for breakfast if between 8am and midnight.
+            // This condition would all arrival time before 1pm.
+            if (timeOfArrivalHr == 12)
+            {
+                cout << "How much did breakfast cost? $";
+                cin >> bfast;
+                lnch = dnr = 0;
+            }
+            // Ask for breakfast and lunch if between 1pm and before 7pm.
+            else if (timeOfArrivalHr >= 1 && timeOfArrivalHr < 7)
+            {
+                cout << "How much did breakfast cost? $";
+                cin >> bfast;
+                cout << "How much did lunch cost? $";
+                cin >> lnch;
+                dnr = 0;
+            }
+            // Ask for breakfast, lunch and dinner if between 7 and before midnight.
+            else if (timeOfArrivalHr >= 7 && timeOfArrivalHr < 12)
+            {
+                cout << "How much did breakfast cost? $";
+                cin >> bfast;
+                cout << "How much did lunch cost? $";
+                cin >> lnch;
+                cout << "How much did dinner cost? $";
+                cin >> dnr;
+            }
+        }
     }
-    if (am_pm_arrive == "pm" || am_pm_arrive == "PM")
-    {
-        if (timeOfArrivalHr == 12)
-        {
-            cout << "How much did breakfast cost on the last day? $";
-            cin >> bfast;
-        }
-        else if (timeOfArrivalHr >= 1 && timeOfArrivalHr < 7)
-        {
-            cout << "How much did breakfast cost on the last day? $";
-            cin >> bfast;
-            cout << "How much did lunch cost on the last day? $";
-            cin >> lnch;
-        }
-        else if (timeOfArrivalHr >= 7 && timeOfArrivalHr < 12)
-        {
-            cout << "How much did breakfast cost on the first day? $";
-            cin >> bfast;
-            cout << "How much did lunch cost on the first day? $";
-            cin >> lnch;
-            cout << "How much did dinner cost on the first day? $";
-            cin >> dnr;
-        }
-        else
-        cout << "Time of arrival was too early for any meals on the last day!";
-    }
+    else 
+        bfast = lnch = dnr = 0;
+    // Call function to calculate allowanaces and exceedance for each meal.
     mealComp(bfast, lnch, dnr, breakfast_total_allowance, breakfast_total_exceeded, 
         lunch_total_allowance, lunch_total_exceeded, dinner_total_allowance,
         dinner_total_exceeded);
 
+    // Calculating totals for allowance and exceedance for the total trip.
     allowanceTotal = breakfast_total_allowance + lunch_total_allowance + dinner_total_allowance;
     allowanceExceeded = breakfast_total_exceeded + lunch_total_exceeded + dinner_total_exceeded;
 }
@@ -328,29 +365,38 @@ void mealComp(double bfast, double lnch, double dnr, double& breakfast_total_all
         double& lunch_total_exceeded, double& dinner_total_allowance,
         double& dinner_total_exceeded)
 {
+    // Declare static constants
     static const double B_ALLOW = 18;
     static const double L_ALLOW = 12;
     static const double D_ALLOW = 20;
 
+    // Check if meal exceeds the allowance
     if ((bfast - B_ALLOW) > 0)
     {
+        // Add the difference to the exceed amount
         breakfast_total_exceeded += (bfast - B_ALLOW);
         breakfast_total_allowance += B_ALLOW;
-    }
-    else   
+    } 
+    else // Otherwise, simply add it to the total allowance
         breakfast_total_allowance += bfast;
+
+    // Check if meal exceeds the allowance
     if ((lnch - L_ALLOW) > 0)
     {
-        breakfast_total_exceeded += (lnch - L_ALLOW); 
-        breakfast_total_allowance += L_ALLOW;
+        // Add the difference to the exceed amount
+        lunch_total_exceeded += (lnch - L_ALLOW); 
+        lunch_total_allowance += L_ALLOW;
     }
-    else   
-        breakfast_total_allowance += lnch;
+    else // Otherwise, simply add it to the total allowance
+        lunch_total_allowance += lnch;
+
+    // Check if meal exceeds the allowance
     if ((dnr - D_ALLOW) > 0)
     {
-        breakfast_total_exceeded += (dnr - D_ALLOW); 
-        breakfast_total_allowance += D_ALLOW;
+        // Add the difference to the exceed amount
+        dinner_total_exceeded += (dnr - D_ALLOW); 
+        dinner_total_allowance += D_ALLOW;
     }
-    else   
-        breakfast_total_allowance += dnr;
+    else // Otherwise, simply add it to the total allowance
+        dinner_total_allowance += dnr;
 }
